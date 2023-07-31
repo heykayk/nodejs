7c4b634040eb64eb8ac8406716b307aed9590a56
heykayk <ngohieu123kayk@gmail.com>
2023-07-31 21:24:59 +0700

    them 1 so thu vien

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ const app = express();
 const port = 3000;
 const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')));
+     app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(
+        app.use(
     express.urlencoded({
         extended: true,
     }),
 );
 
-app.use(express.json());
+    pp.use(express.json());
 // HTTP logger
 app.use(morgan('combined'));
 
